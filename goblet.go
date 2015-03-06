ec8209492fad3875ab11f43810371d932ddb625c
Cedric Darne <cedric.darne@gmail.com>
2015-03-06 14:16:37 -0500

    Refactored w/ mow.cli

--- goblet.go
+++ goblet.go
@@ -5,44 +5,59 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/cdarne/goblet/goblet"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jawher/mow.cli"
 )
 
 func main() {
-	cnf, err := goblet.LoadDBConfig("db/config.json")
-	if err != nil {
-		log.Fatalln(err)
-	}
+	app := cli.App("goblet", "Tests framework utility")
+	app.Command("db", "Database jobs", func(dbCmd *cli.Cmd) {
+		dbCmd.Command("create", "Create test DB", dbCreate)
+	})
+	app.Run(os.Args)
+}
 
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s(%s:%d)/%s?charset=latin1", cnf.User, cnf.Password, cnf.Protocol, cnf.Host, cnf.Port, "mysql"))
-	if err != nil {
-		log.Fatalf("Could not connect to DB: %s\n", err)
-	}
-	defer db.Close()
+func dbCreate(cmd *cli.Cmd) {
+	dbConfig := cmd.StringOpt("c config", "db/config.json", "Path to the DB config file")
+	dbSchema := cmd.StringOpt("s schema", "db/schema.sql", "Path to the DB schema file")
 
-	stmts := []string{fmt.Sprintf("DROP DATABASE %s", cnf.Database), fmt.Sprintf("CREATE DATABASE %s", cnf.Database)}
-	err = execMulti(db, stmts)
-	if err != nil {
-		log.Fatalf("Error while executing query: %s\n", err)
-	}
+	cmd.Action = func() {
+		cnf, err := goblet.LoadDBConfig(*dbConfig)
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-	testDb, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s(%s:%d)/%s?charset=latin1", cnf.User, cnf.Password, cnf.Protocol, cnf.Host, cnf.Port, cnf.Database))
-	if err != nil {
-		log.Fatalf("Could not connect to test DB: %s\n", err)
-	}
-	defer testDb.Close()
+		db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s(%s:%d)/%s?charset=latin1", cnf.User, cnf.Password, cnf.Protocol, cnf.Host, cnf.Port, "mysql"))
+		if err != nil {
+			log.Fatalf("Could not connect to DB: %s\n", err)
+		}
+		defer db.Close()
 
-	data, err := ioutil.ReadFile("db/schema.sql")
-	if err != nil {
-		log.Fatalf("Could not read the DB schema file: %s\n", err)
-	}
-	stmts = strings.Split(string(data), ";")
-	err = execMulti(testDb, stmts)
-	if err != nil {
-		log.Fatalf("Error while loading the DB schema: %s\n", err)
+		stmts := []string{fmt.Sprintf("DROP DATABASE %s", cnf.Database), fmt.Sprintf("CREATE DATABASE %s", cnf.Database)}
+		err = execMulti(db, stmts)
+		if err != nil {
+			log.Fatalf("Error while executing query: %s\n", err)
+		}
+
+		testDb, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s(%s:%d)/%s?charset=latin1", cnf.User, cnf.Password, cnf.Protocol, cnf.Host, cnf.Port, cnf.Database))
+		if err != nil {
+			log.Fatalf("Could not connect to test DB: %s\n", err)
+		}
+		defer testDb.Close()
+
+		data, err := ioutil.ReadFile(*dbSchema)
+		if err != nil {
+			log.Fatalf("Could not read the DB schema file: %s\n", err)
+		}
+		stmts = strings.Split(string(data), ";")
+		err = execMulti(testDb, stmts)
+		if err != nil {
+			log.Fatalf("Error while loading the DB schema: %s\n", err)
+		}
 	}
 }
 
